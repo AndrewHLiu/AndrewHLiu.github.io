ecf6831221e250bb8c8f61472259b5b6953f0d3f
Andrew Liu <ahliu@berkeley.edu>
2015-11-13 02:49:35 -0800

    added a functionality to track hits

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+function notifyServer(){
+	var fb = new Firebase("https://andrewliu.firebaseio.com/");
+	fb.authAnonymously(sendNotification);
+
+	root = fb.root();
+	var d = new Date();
+	var n = d.getMonth();
+	var y = d.getFullYear();
+	x = root.child("Hits").child(y.toString()+"-"+n.toString());
+	x.transaction(updateData);
+}
+
+function sendNotification(){
+
+}
+
+function updateData(prevData){
+	prevData=prevData+1;
+	return prevData;
+}
+
+$( document ).ready(function() {
+    notifyServer();
+  });
